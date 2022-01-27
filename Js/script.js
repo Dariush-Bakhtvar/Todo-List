8a60a480684f4f66e4c8b2c70b7d7b718aee0e5b
Dariush <d.bakhtvar@gmail.com>
2022-01-27 14:06:53 +0330

    edite style2 05

--- Js/script.js
+++ Js/script.js
@@ -5,6 +5,7 @@ const createTask = document.querySelector('[data-createTask]');
 const toDoWapper = document.querySelector('.task-wrapper');
 const taskList = document.querySelector('.tasklist a');
 const subList = document.querySelector('.sublist');
+const accord = document.querySelector('[data-accordion]');
 Pin.addEventListener('click', () => {
     const sidebar = document.querySelector('.sidebar');
     const wrapper = document.querySelector('.task-wrapper');
@@ -28,4 +29,11 @@ taskList.addEventListener('click', () => {
     const arrow = document.querySelector('.arrow');
     subList.classList.toggle('active');
     arrow.classList.toggle('rotate');
+});
+accord.addEventListener('click', (e) => {
+    const target = e.target.closest('a');
+    const nextSibl = target.nextElementSibling;
+    if (nextSibl.style.maxHeight) nextSibl.style.maxHeight = null;
+    else nextSibl.style.maxHeight = `${nextSibl.scrollHeight}px`;
+
 });
